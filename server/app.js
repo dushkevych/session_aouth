a897bf3296886ebef187eaea590a914ee816e544
lemishov <lemishov@gmail.com>
2020-09-17 23:26:02 +0300

    connected mongoDB
    created first version of User model

--- server/app.js
+++ server/app.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 const port = 3000
+
+const uri = "mongodb+srv://saas_backend_1412:V11S5OY5Kq7w1s9e@cluster0.syqmf.mongodb.net/saas_startup?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('MongoDB Connected…')
+})
+.catch(err => console.log(err))
 
 app.use('/', express.static('./dist', {
   index: "index.html"
