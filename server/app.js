a9f47a4fa3ba359cf5159351cff2849f466339c3
lemishov <lemishov@gmail.com>
2020-09-24 15:05:36 +0300

    error handler added

--- server/app.js
+++ server/app.js
@@ -29,4 +29,24 @@ app.use('/', express.static('./dist', {
 
 app.use('/', userRoutes, loginRoute);
 
+// catch error and forward to error handler
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+// error handler
+app.use((error, req, res, next) => {
+// return json with error info
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message : error.message
+    }
+  });
+});
+
+module.exports = app;
+
 app.listen(port, () => console.log(`App listening on port ${port}!`))
