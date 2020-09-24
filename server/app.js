a0e8dac7ec679568d5830dcdd491f5f4e04cb745
lemishov <lemishov@gmail.com>
2020-09-24 22:38:12 +0300

    Mangoose connect function moved to config folder from app.js

--- server/app.js
+++ server/app.js
@@ -6,21 +6,10 @@ const port = 3000
 const dbConnect = require('../server/config/db-connection');
 const userRoutes = require('./routes/users');
 const loginRoute = require('./routes/login')
+
 //DB Config
-// const uri = "mongodb+srv://saas_backend_1412:V11S5OY5Kq7w1s9e@cluster0.syqmf.mongodb.net/saas_startup?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
 
-const connection =  dbConnect.dbConnect;
-
-// Connect to Mongo
-// mongoose.connect(uri, {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useUnifiedTopology: true
-// })
-// .then(() => {
-//   console.log('MongoDB Connected…')
-// })
-// .catch(err => console.log(err))
+dbConnect()
 
 //routes
 app.use('/', express.static('./dist', {
--- server/config/db-connection.js
+++ server/config/db-connection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-async function dbConnect() {
+const dbConnect = async function () {
     try {
-    await mongoose.connect(process.env.DB_CONNECTION_URI, {
+    await mongoose.connect(//process.env.DB_CONNECTION_URI
+        "mongodb+srv://saas_backend_1412:V11S5OY5Kq7w1s9e@cluster0.syqmf.mongodb.net/saas_startup?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+        , {
         useNewUrlParser: true,
         useCreateIndex: true,
         useUnifiedTopology: true
@@ -11,7 +13,7 @@ async function dbConnect() {
     }catch(e) {
         console.log('DB CONNECTION ERROR');
         throw e;
-    }
-}
+    };
+};
 
-module.exports.default = dbConnect;
+module.exports = dbConnect;
