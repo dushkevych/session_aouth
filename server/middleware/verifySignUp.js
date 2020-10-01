21611c72485ebec0fa972d2fd238cff45fc3ada5
lemishov <lemishov@gmail.com>
2020-10-01 18:17:50 +0300

    verifySignUp.js refactoring

--- server/middleware/verifySignUp.js
+++ server/middleware/verifySignUp.js
@@ -1,24 +1,29 @@
 const User = require("../models/User")
 
-checkDuplicateUsernameOrEmail = (req, res, next) => {
-  // Username
-  User.findOne({
-    username: req.body.username
-  }).exec((err, user) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
-    }
-
-    if (user) {
-      res.status(400).send({ message: "Failed! Username is already in use!" });
-      return;
-    }
+const checkDuplicateUsernameOrEmail = async (req, res, next) => {
+    // Username
+    try{
+        const user = await User.findOne({
+            username: req.body.username
+          });
+        user.exec((err, user) => {
+            if (err) {
+              res.status(500).send({ message: err });
+              return;
+            }
+        
+            if (user) {
+              res.status(400).send({ message: "Failed! Username is already in use!" });
+              return;
+            };
+        });
+        
 
     // Email
-    User.findOne({
+    const email = await User.findOne({
       email: req.body.email
-    }).exec((err, user) => {
+    });
+    email.exec((err, user) => {
       if (err) {
         res.status(500).send({ message: err });
         return;
@@ -31,8 +36,12 @@ checkDuplicateUsernameOrEmail = (req, res, next) => {
 
       next();
     });
-  });
-};
+  
+  } catch (err) {
+    console.log('DB CONNECTION ERROR');
+    throw err;
+  };
+}; 
 
 const verifySignUp = {
   checkDuplicateUsernameOrEmail
