758d9578abd5b4749934231a341a588d0b63c274
lemishov <lemishov@gmail.com>
2020-11-03 23:02:47 +0200

    auth middleware fixed - working version

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -2,10 +2,11 @@ const User = require('../models/User');
 
 exports.user = async (req, res, next) => {
     try {
-      const { id } = req.user;
+      const { email } = req.body;
 
-      const user = await User.findById(id).select('-password').exec();
-  
+      //const user = await User.findOne(email).select('-password').exec();
+      const user = await User.findOne({ email }).select('-password').exec(); 
+
       return res.json({success: true, user});
     } catch (err) {
       return next(err);
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,14 +3,8 @@ const jwt = require('jsonwebtoken');
 const auth = async (req, res, next) => {
     try {
 
-       // const {userId} =await req.session;
-        console.log('1',req.session.id)
-        // console.log('2',req.sessionId)
-        console.log('3',req.session.userId)
-        //const token = await req.headers["x-access-token"];
-
-        //Check for token
-        if (!req.session.userId) {
+         //Check for session
+        if (!req.session.user) {
             //return res.status(401).json({ message: 'No token, authorisation denied'})
             return res.status(401).json({ message: 'Session is not active, authorization denied'})
         };
