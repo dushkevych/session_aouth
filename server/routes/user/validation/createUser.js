15b0d786aba864abc7817cd165bfa229912dc007
lemishov <lemishov@gmail.com>
2020-11-12 17:34:56 +0200

    signUp validation added

--- server/routes/user/validation/createUser.js
+++ server/routes/user/validation/createUser.js
@@ -1,9 +1,29 @@
-const { check, body, param, query, isEmail, isLength,  validationResult, withMessage } = require('express-validator');
+const { check, validationResult, /*body, param, query, isEmail, isLength,  withMessage*/ } = require('express-validator');
 
 exports.signUpValidationChain = [
-    check('email').isEmail().withMessage('e-mail is required'),
-    
-    check('password').isLength({ min: 6 }).withMessage('password must be 6 characters')
+    check('firstName')
+      .not().isEmpty()
+      .trim()
+      .withMessage('First Name is required'),
+
+    check('lastName')
+      .not().isEmpty()
+      .trim()
+      .withMessage('Last Name is required'),
+
+    check('email')
+      .not().isEmpty()
+      .isEmail()
+      .normalizeEmail()
+      .trim()
+      .withMessage('valid e-mail is required'),
+          
+    check('password')
+      .isLength({ min: 8 }).withMessage('password must be 8 characters')
+      //password must be at least 8 characters long, contain uppercase letters, lowercase letters, numbers, can contain special characters
+      .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)
+      .trim().escape()
+      .withMessage('password must be at least 8 characters long, contain uppercase letters, lowercase letters, numbers, can contain special characters')
   ]
 
 exports.validateSignUp = (req, res, next) => {
